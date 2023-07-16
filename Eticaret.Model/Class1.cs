2d6256ec44dfc1da8c58998d7c46198ff2c4e6a7
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-07-16 15:24:40 +0300

    Add project files.

---
+++ Eticaret.Model/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Eticaret.Model
+{
+    [Table("tblKategori")]
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public int UstKategoriId { get; set; }
+        public int Sira { get; set; }
+        public string Foto { get; set; }
+        public bool Aktif  { get; set; }
+
+       
+
+
+    }
+}
